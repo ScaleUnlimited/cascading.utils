2ac9e7e26a72c8041779cf4e5556b9d9c7d5f48d
Chris Schneider <Schmed@TransPac.com>
2011-02-18 13:57:03 -0800

    Version 1.0 (deployed to Conjars) * Removed avro dependency, since this pulls in jetty 6.1.14, which conflicts the jetty-5.1.10 bixo uses for testing. This meant commenting out DatumCompiler(Test). * Put install task back in. * Also created a deploy task, but it uses the scp optional task (which depends on jsch.jar). Actually, I never did get it to work, even after installing jsch.jar in my $ANT_HOME/lib/, but I left the task in there in case we want to revisit this later.

--- src/test/java/com/bixolabs/cascading/DatumCompilerTest.java
+++ src/test/java/com/bixolabs/cascading/DatumCompilerTest.java
@@ -11,16 +11,16 @@ public class DatumCompilerTest {
 
     @Test
     public void testSimpleSchema() throws Exception {
-        File src = new File("src/test/resources/schemas/simple.avsc");
-        File dst = new File("build/test/DatumCompilerTest/testSimpleSchema/");
-        dst.mkdirs();
-        
-        File outputFile = new File(dst, "com/bixolabs/cascading/SimpleDatum.java");
-        outputFile.delete();
-        
-        DatumCompiler.compileSchema(DatumCompiler.BASE_DATUM_CLASSNAME, src, dst);
-        
-        String result = IOUtils.toString(new FileInputStream(outputFile));
-        System.out.println(result);
+//        File src = new File("src/test/resources/schemas/simple.avsc");
+//        File dst = new File("build/test/DatumCompilerTest/testSimpleSchema/");
+//        dst.mkdirs();
+//        
+//        File outputFile = new File(dst, "com/bixolabs/cascading/SimpleDatum.java");
+//        outputFile.delete();
+//        
+//        DatumCompiler.compileSchema(DatumCompiler.BASE_DATUM_CLASSNAME, src, dst);
+//        
+//        String result = IOUtils.toString(new FileInputStream(outputFile));
+//        System.out.println(result);
     }
 }
