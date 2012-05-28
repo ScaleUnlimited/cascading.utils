c63fe09b7bd1f6fbc45545e6725edb6241307424
Ken Krugler <ken@transpac.com>
2012-05-27 18:29:45 -0700

    Add StdDeviation Aggregator

---
+++ src/test/java/com/bixolabs/cascading/StdDeviationTest.java
@@ -0,0 +1,78 @@
+package com.bixolabs.cascading;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.apache.hadoop.mapred.JobConf;
+import org.junit.Test;
+
+import cascading.flow.Flow;
+import cascading.flow.FlowConnector;
+import cascading.pipe.Every;
+import cascading.pipe.GroupBy;
+import cascading.pipe.Pipe;
+import cascading.scheme.SequenceFile;
+import cascading.tap.Lfs;
+import cascading.tap.SinkMode;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntry;
+import cascading.tuple.TupleEntryCollector;
+import cascading.tuple.TupleEntryIterator;
+
+public class StdDeviationTest {
+
+    @Test
+    public void test() throws Exception {
+        // Set up input tuples that have two groups, each with 10K values.
+        
+        final Fields groupField = new Fields("user");
+        final Fields testFields = new Fields("user", "value");
+        
+        String in = "build/test/StatisticsTest/test/in";
+        String out = "build/test/GroupLimitTest/test/out";
+
+        Lfs sourceTap = new Lfs(new SequenceFile(testFields), in, SinkMode.REPLACE);
+        TupleEntryCollector write = sourceTap.openForWrite(new JobConf());
+        
+        Random gen = new Random(1L);
+        for (int i = 0; i < 10000; i++) {
+            write.add(new Tuple("user1", gen.nextGaussian()));
+        }
+        
+        gen = new Random(1L);
+        for (int i = 0; i < 10000; i++) {
+            write.add(new Tuple("user2", gen.nextGaussian()));
+        }
+        write.close();
+
+        Pipe pipe = new Pipe("test");
+        pipe = new GroupBy(pipe, groupField);
+        pipe = new Every(pipe, new Fields("value"), new StdDeviation(), Fields.ALL);
+        
+        Lfs sinkTap = new Lfs(new SequenceFile(new Fields("user", StdDeviation.FIELD_NAME)), out, SinkMode.REPLACE);
+        
+        Flow flow = new FlowConnector().connect(sourceTap, sinkTap, pipe);
+        flow.complete();
+        
+        TupleEntryIterator iter = sinkTap.openForRead(new JobConf());
+
+        TupleEntry te = iter.next();
+        assertEquals("user1", te.getString("user"));
+        double stdDeviation = te.getDouble(StdDeviation.FIELD_NAME);
+        assertTrue(stdDeviation >= 0.988395);
+        assertTrue(stdDeviation <= 1.011883);
+        
+        
+        te = iter.next();
+        assertEquals("user2", te.getString("user"));
+        stdDeviation = te.getDouble(StdDeviation.FIELD_NAME);
+        assertTrue(stdDeviation >= 0.988395);
+        assertTrue(stdDeviation <= 1.011883);
+        
+        assertFalse(iter.hasNext());
+    
+    }
+
+}
